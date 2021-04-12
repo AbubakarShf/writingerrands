c56f850812ee32d400c0b3eb360bb5475b11a0d4
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-04-12 08:08:07 +0500

    writingerrands(Commit_v1)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Styled from "styled-components";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello there!</h1>
+        </>
+    );
+};
+
+export default App;
